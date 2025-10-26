49b1d573289714211afd6c8a550da1a071a97417
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2025-10-26 16:04:58 +0000

    test

--- action-src/src/git.ts
+++ action-src/src/git.ts
@@ -54,7 +54,7 @@ export async function gitListFilesForPullRequest(pr: PullRequestEvent): Promise<
         await deepenIfNecessary(commitCount + 1);
         return gitListFiles(sha1, sha2);
     } catch (e) {
-        throw new GitError(`Error getting files for PR ${pr?.number} from git`, e);
+        throw new GitError(`Error getting files for PR ${pr?.number} from git ${e.message}`, e);
     }
 }
 
